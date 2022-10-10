cc15c3408f875764d51bd13cd7cec96883fd4876
Loc Educate <loceducate@gmail.com>
2022-10-10 12:01:13 +0700

    Learn the basic of os.Args

--- main.go
+++ main.go
@@ -7,4 +7,11 @@ import (
 
 func main() {
 	fmt.Printf("%#v\n", os.Args)
+
+	fmt.Println("Path: ", os.Args[0])
+	fmt.Println("1st argument: ", os.Args[1])
+	fmt.Println("2nd argument: ", os.Args[2])
+	fmt.Println("3rd argument: ", os.Args[3])
+
+	fmt.Println("Number of items indside os.Args: ", len(os.Args))
 }
